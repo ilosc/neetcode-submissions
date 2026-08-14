a44ceba895dd8ae33465929aa5d797854efcaa73
ílosc <1476079+ilosc@users.noreply.github.com>
2026-08-14 22:18:21 +0100

    Remove: two-integer-sum - submission-0

--- Data Structures & Algorithms/two-integer-sum/submission-0.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i,vi in enumerate(nums):
-            for j,vj in enumerate(nums):
-                if ((vi + vj) == target) and (i != j):
-                    return [i, j,]
