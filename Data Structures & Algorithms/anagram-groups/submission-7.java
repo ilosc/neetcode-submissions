15945a1c12bb88ca9ca52ce5f926e2a167f6cbf0
ílosc <1476079+ilosc@users.noreply.github.com>
2026-08-14 22:13:58 +0100

    Add: anagram-groups - submission-7

---
+++ Data Structures & Algorithms/anagram-groups/submission-7.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> result = new ArrayList<>();
+        HashMap<Integer, Integer> previous = new HashMap<>();
+        for (String word: strs) {
+            int[] counter = new int[26];
+            for (char c: word.toCharArray()) {
+                counter[c - 'a']++;
+            }
+            var key = Arrays.hashCode(counter);
+            var idx = previous.get(key);
+            if (idx == null) {
+                var list = new ArrayList<String>();
+                list.add(word);
+                result.add(list);
+                previous.put(key, result.size() - 1);
+            } else {
+                result.get(idx).add(word);
+            }
+        }
+
+        return result;        
+    }
+}
