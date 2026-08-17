c51d440ee41789ca81df235a5a7ff672090dddcb
ílosc <1476079+ilosc@users.noreply.github.com>
2026-08-17 12:52:15 +0100

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        board_rows = defaultdict(set)
+        board_cols = defaultdict(set)
+        quadrants = defaultdict(list)
+        for i, row in enumerate(board):
+            for j, value in enumerate(row):
+                if value == '.':
+                    continue
+
+                value = int(value)
+
+                if value in board_rows[i]:
+                    return False
+                board_rows[i].add(value)
+
+                if value in board_cols[j]:
+                    return False
+                board_cols[j].add(value)
+
+                idx = ((i // 3), (j // 3))
+                if value in quadrants[idx]:
+                    return False
+                quadrants[idx].append(value)
+        return True
+
+
+                
