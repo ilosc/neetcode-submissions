f058fae4c32ef03cb2fb2361e7b0162d638a05a7
ílosc <1476079+ilosc@users.noreply.github.com>
2026-08-18 22:38:38 +0100

    Add: longest-consecutive-sequence - submission-2

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-2.py
@@ -0,0 +1,36 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        lists = []
+        table = {}
+
+        max_length = 0
+        mod_list = None
+        for n in set(nums):
+            idx_list_prev = table.get(n - 1)
+            idx_list_post = table.get(n + 1)
+            if (idx_list_prev is None) and (idx_list_post is None):
+                mod_list = [n]
+                lists.append(mod_list)
+                table[n] = len(lists) - 1
+            elif (idx_list_prev is not None) and (idx_list_post is not None):
+                mod_list = lists[idx_list_prev]
+                mod_list.extend(lists[idx_list_post])
+                # for item in lists[idx_list_post]:
+                #    mod_list.append(item)
+                #    table[item] = table[n - 1]
+                mod_list.append(n)                    
+                table[n] = idx_list_prev
+            elif (idx_list_prev is None):
+                table[n] = idx_list_post
+                mod_list = lists[idx_list_post]
+                mod_list.append(n)
+            else:
+                table[n] = idx_list_prev
+                mod_list = lists[idx_list_prev]
+                mod_list.append(n)
+
+            list_len = len(mod_list)
+            if list_len > max_length:
+                max_length = list_len
+
+        return max_length
