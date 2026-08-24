c6511e030e88dd45ca848490427a388b85b7f5de
ílosc <1476079+ilosc@users.noreply.github.com>
2026-08-24 01:09:10 +0100

    Add: car-fleet - submission-4

---
+++ Data Structures & Algorithms/car-fleet/submission-4.py
@@ -0,0 +1,16 @@
+class Solution:
+	def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+		data = []
+		for i, pos in enumerate(position):
+			data.append((pos, speed[i]))
+		data.sort(key=lambda t: t[0], reverse=True)
+
+		prev_times = None
+		n_fleets = 0
+		for d in data:
+			times = (target - d[0]) / d[1]
+			if (prev_times is None) or (times > prev_times):
+				n_fleets += 1
+				prev_times = times
+				
+		return n_fleets
