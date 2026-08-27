de2bc53484e4fec032d9dba00dbe3ae98165e1f1
ílosc <1476079+ilosc@users.noreply.github.com>
2026-08-27 13:18:48 +0100

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,21 @@
+class Solution:
+	def isPalindrome(self, s: str) -> bool:
+		alphabet = "abcdefghijklmnopqrstuvwxyz0123456789"
+		length = len(s)
+		p_init, p_end = 0, length - 1
+		while p_init < p_end:
+			ci = s[p_init].lower()
+			if ci not in alphabet:
+				p_init += 1
+				if s[p_end].lower() not in alphabet:
+					p_end -= 1
+				continue
+			ce = s[p_end].lower()
+			if ce not in alphabet:
+				p_end -= 1
+				continue
+			if ci != ce:
+				return False
+			p_init += 1
+			p_end -= 1
+		return True
